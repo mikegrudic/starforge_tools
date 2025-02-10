abcd3420099c13ab8febf202bd7fe20721530117
mikegrudic <2715993+mikegrudic@users.noreply.github.com>
2025-02-10 16:57:30 +0000

    :art: Format Python code with psf/black

--- initial_conditions/plummer.py
+++ initial_conditions/plummer.py
@@ -8,52 +8,62 @@ from sys import argv
 
 G = 1
 
-if len(argv)>1:
-    N = int(float(argv[1])+0.5)
+if len(argv) > 1:
+    N = int(float(argv[1]) + 0.5)
 else:
     N = 32**3
-if len(argv)>1:
+if len(argv) > 1:
     m = float(argv[2])
 else:
-    m = 1.
-if len(argv)>2:
+    m = 1.0
+if len(argv) > 2:
     a = float(argv[3])
 else:
-    a = 1.
+    a = 1.0
 
-x= np.arange(0,1,1./N) + np.random.rand(N)/N
+x = np.arange(0, 1, 1.0 / N) + np.random.rand(N) / N
 print(x.max())
-r = np.sqrt( x**(2./3) * (1+x**(2./3) + x**(4./3))/(1-x**2))
+r = np.sqrt(x ** (2.0 / 3) * (1 + x ** (2.0 / 3) + x ** (4.0 / 3)) / (1 - x**2))
 phi = np.random.rand(N) * 2 * np.pi
-theta = np.arccos(2.0*np.random.rand(N)-1.0)
+theta = np.arccos(2.0 * np.random.rand(N) - 1.0)
 
-x = np.c_[r*np.cos(phi)*np.sin(theta), r*np.sin(phi)*np.sin(theta), r*np.cos(theta)]
+x = np.c_[r * np.cos(phi) * np.sin(theta), r * np.sin(phi) * np.sin(theta), r * np.cos(theta)]
 
-phi = -(1+r**2)**-0.5
-v_e = (-2*phi)**0.5
+phi = -((1 + r**2) ** -0.5)
+v_e = (-2 * phi) ** 0.5
 
-cdf = lambda Rp, rr: (2*(Rp*np.sqrt(1 - Rp**2)*(-105 + 1210*Rp**2 - 2104*Rp**4 + 1488*Rp**6 - 384*Rp**8) + 105*np.arcsin(Rp)))/(105.*np.pi) - rr
+cdf = (
+    lambda Rp, rr: (
+        2
+        * (
+            Rp * np.sqrt(1 - Rp**2) * (-105 + 1210 * Rp**2 - 2104 * Rp**4 + 1488 * Rp**6 - 384 * Rp**8)
+            + 105 * np.arcsin(Rp)
+        )
+    )
+    / (105.0 * np.pi)
+    - rr
+)
 
-Qs = np.array([brentq(cdf,0,1,args=(R,)) for R in np.random.rand(N)])
+Qs = np.array([brentq(cdf, 0, 1, args=(R,)) for R in np.random.rand(N)])
 
 v = v_e * Qs
 
-print(np.sum(v**2 / 2/ N), np.sum(phi)/2/N)
+print(np.sum(v**2 / 2 / N), np.sum(phi) / 2 / N)
 phi_v = np.random.rand(N) * 2 * np.pi
-theta_v = np.arccos(2.0*np.random.rand(N)-1.0)
+theta_v = np.arccos(2.0 * np.random.rand(N) - 1.0)
 
-v = np.c_[v*np.cos(phi_v)*np.sin(theta_v), v*np.sin(phi_v)*np.sin(theta_v), v*np.cos(theta_v)]
-boxsize = 10.
-F = h5py.File("plummer_%d_m%g_a%g.hdf5"%(int(N**(1./3)+0.5),m, a),'w')
+v = np.c_[v * np.cos(phi_v) * np.sin(theta_v), v * np.sin(phi_v) * np.sin(theta_v), v * np.cos(theta_v)]
+boxsize = 10.0
+F = h5py.File("plummer_%d_m%g_a%g.hdf5" % (int(N ** (1.0 / 3) + 0.5), m, a), "w")
 F.create_group("PartType1")
 F.create_group("Header")
-F["Header"].attrs["NumPart_ThisFile"] = [0,len(x),0,0,0,0]
-F["Header"].attrs["MassTable"] = [0,float(m)/N,0,0,0,0]
+F["Header"].attrs["NumPart_ThisFile"] = [0, len(x), 0, 0, 0, 0]
+F["Header"].attrs["MassTable"] = [0, float(m) / N, 0, 0, 0, 0]
 F["Header"].attrs["BoxSize"] = boxsize
 F["Header"].attrs["Time"] = 0.0
-F["PartType1"].create_dataset("Coordinates", data=x*a + boxsize/2)
-F["PartType1"].create_dataset("Potential", data=phi*float(m)/a)
-F["PartType1"].create_dataset("Velocities", data=v*(float(m)/a)**0.5 * G**0.5)
-F["PartType1"].create_dataset("Masses", data=np.repeat(float(m)/N,N))
+F["PartType1"].create_dataset("Coordinates", data=x * a + boxsize / 2)
+F["PartType1"].create_dataset("Potential", data=phi * float(m) / a)
+F["PartType1"].create_dataset("Velocities", data=v * (float(m) / a) ** 0.5 * G**0.5)
+F["PartType1"].create_dataset("Masses", data=np.repeat(float(m) / N, N))
 F["PartType1"].create_dataset("ParticleIDs", data=np.arange(N))
 F.close()

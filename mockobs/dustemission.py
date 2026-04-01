53642ec8b0b130456a06dee549e459c43fd79a1a
tobin-wainer <tobinwainer@gmail.com>
2026-03-31 20:07:42 -0400

    Change of dust emission work flow for directions

--- mockobs/dustemission.py
+++ mockobs/dustemission.py
@@ -55,22 +55,89 @@ DIR = options["--dir"]
 
 SOLAR_Z = 0.0142
 
-def transform_coordinates(x, direction):
-    """Transforms coordinates based on the specified direction"""
-    if direction == 'x':
-        return x[:, [1, 2, 0]]
-    elif direction == 'y':
-        return x[:, [0, 2, 1]]
-    elif direction == 'z':
-        return x
+
+def parse_dir_option(dir_option):
+    """
+    Parse the --dir option to return a 3-vector.
+    Accepts 'x', 'y', 'z', or a comma-separated vector (e.g. '1,1,0').
+    """
+    if dir_option in ['x', 'y', 'z']:
+        mapping = {'x': np.array([1, 0, 0]),
+                   'y': np.array([0, 1, 0]),
+                   'z': np.array([0, 0, 1])}
+        return mapping[dir_option]
     else:
-        raise ValueError(f"Invalid direction: {direction}")
+        # Assume comma-separated vector, e.g. "1,0,0"
+        return np.array([float(x) for x in dir_option.split(',')])
+
+def rotate_3d(coords, view_vector, center=None):
+    """
+    Rotate coords so view_vector aligns with z-axis, around a specified center.
+
+    Parameters
+    ----------
+    coords : ndarray
+        Nx3 array of coordinates.
+    view_vector : array-like
+        3-element vector to align with the z-axis.
+    center : array-like or None
+        3-element array, the center point to rotate around (default: origin).
+
+    Returns
+    -------
+    rotated_coords : ndarray
+        Rotated coordinates.
+    """
+    coords = np.asarray(coords)
+    if center is not None:
+        center = np.asarray(center)
+        coords_shifted = coords - center
+    else:
+        coords_shifted = coords
+
+    view_vector = view_vector / np.linalg.norm(view_vector)
+    z_axis = np.array([0, 0, 1])
+    v = np.cross(view_vector, z_axis)
+    s = np.linalg.norm(v)
+    c = np.dot(view_vector, z_axis)
+    I = np.eye(3)
+    if s != 0:
+        vx = np.array([[0, -v[2], v[1]],
+                       [v[2], 0, -v[0]],
+                       [-v[1], v[0], 0]])
+        R = I + vx + np.dot(vx, vx) * ((1 - c) / (s ** 2))
+    else:
+        R = I
+    rotated = np.dot(coords_shifted, R.T)
+    if center is not None:
+        rotated = rotated + center
+    return rotated
+
+def transform_coordinates(x, direction, center):
+    """
+    Transforms coordinates based on the specified direction or vector.
+
+    Parameters
+    ----------
+    x : ndarray
+        Nx3 array of coordinates.
+    direction : str
+        'x', 'y', 'z', or a comma-separated vector (e.g. '1,1,0').
+    center : array-like
+        3-element array, the center point to rotate around.
+
+    Returns
+    -------
+    ndarray
+        Transformed coordinates.
+    """
+    view_vector = parse_dir_option(direction)
+    return rotate_3d(x, view_vector, center)
 
 def make_dustemission_map_from_snapshot(path):
     """Makes a dust emission map from a STARFORGE snapshot"""
     with h5py.File(path, "r") as F:
         x = np.float32(F["PartType0/Coordinates"][:])
-        x = transform_coordinates(x, DIR)
         m = np.float32(F["PartType0/Masses"][:])
         h = np.float32(F["PartType0/SmoothingLength"][:])
         Z = F["PartType0/Metallicity"][:]
@@ -96,6 +163,9 @@ def make_dustemission_map_from_snapshot(path):
         center = 0.5 * np.array(3 * [boxsize])
 
     dx = size / (RES - 1)
+    
+    x = transform_coordinates(x, DIR, center)
+    
     intensity = dust_emission_map(x, m * Z, h, Tdust, size, RES, WAVELENGTHS, center)
 
     # add noise
@@ -126,29 +196,29 @@ def make_dustemission_map_from_snapshot(path):
         #        F.create_dataset("Intensity_noise_cgs", data=noise)
         F.create_dataset("SurfaceDensity_Msun_pc2", data=sigmagas)
 
-        if len(WAVELENGTHS) >= 3:
-            # fit each pixel to a modified blackbody as an observer would
-            params = modified_blackbody_fit_image(intensity, 0.1*intensity, WAVELENGTHS)
-            tau0, beta, Tdust_fit = (
-                params[:, :, 0],
-                params[:, :, 1],
-                params[:, :, 2],
-            )
+        # if len(WAVELENGTHS) >= 3:
+        #     # fit each pixel to a modified blackbody as an observer would
+        #     params = modified_blackbody_fit_image(intensity, 0.1*intensity, WAVELENGTHS)
+        #     tau0, beta, Tdust_fit = (
+        #         params[:, :, 0],
+        #         params[:, :, 1],
+        #         params[:, :, 2],
+        #     )
 
-            F.create_dataset("Fit_Tau500um", data=tau0)
-            F.create_dataset("Fit_Beta", data=beta)
-            F.create_dataset("Fit_Tdust", data=Tdust_fit)
-            F.create_dataset("Tdust_massweighted", data=Tdust_avg)
+        #     F.create_dataset("Fit_Tau500um", data=tau0)
+        #     F.create_dataset("Fit_Beta", data=beta)
+        #     F.create_dataset("Fit_Tdust", data=Tdust_fit)
+        #     F.create_dataset("Tdust_massweighted", data=Tdust_avg)
 
-            params_avg = modified_blackbody_fit_gaussnewton(
-                intensity.mean((0, 1)), 0.1*intensity.mean((0,1)),WAVELENGTHS
-            )
+        #     params_avg = modified_blackbody_fit_gaussnewton(
+        #         intensity.mean((0, 1)), 0.1*intensity.mean((0,1)),WAVELENGTHS
+        #     )
 
-            tau0, beta, Tdust_fit = params_avg
+        #     tau0, beta, Tdust_fit = params_avg
 
-            F.create_dataset("SEDFit_Tau500um", data=tau0)
-            F.create_dataset("SEDFit_Beta", data=beta)
-            F.create_dataset("SEDFit_Tdust", data=Tdust_fit)
+        #     F.create_dataset("SEDFit_Tau500um", data=tau0)
+        #     F.create_dataset("SEDFit_Beta", data=beta)
+        #     F.create_dataset("SEDFit_Tdust", data=Tdust_fit)
 
 
 def main():
